762510a4d923142856afa79018d36c3755a48645
Arpit <arpitkumar3203@gmail.com>
2024-07-29 11:06:21 +0530

    added stocktracker view

--- stock_stream/base/views.py
+++ stock_stream/base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from yahoo_fin.stock_info import tickers_nifty50
 
 # Create your views here.
 def stockpicker(request):
-    return render(request, 'base/stockpicker.html')
+    stock_picker = tickers_nifty50()
+    # print(stock_picker, flush=True) # working fine
+    context = {
+        'stockpicker': stock_picker
+    }
+    return render(request, 'base/stockpicker.html', context=context)
 
 def stocktracker(request):
+    print(request.GET.getlist('stockpicker'))
     return render(request, 'base/stocktracker.html')
